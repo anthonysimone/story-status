d78c1e3db4943b520aba28716ac912fdc98896f8
Anthony Simone <anthony.simone7@gmail.com>
2019-12-12 14:36:28 -0800

    Save progress

--- .storybook/addons/story-status.js
+++ .storybook/addons/story-status.js
@@ -1,33 +1,95 @@
 //.storybook/addons/story-status.js
 import React, { Fragment } from "react";
-import { AddonPanel } from "@storybook/components";
+import { AddonPanel, Badge, WithTooltip } from "@storybook/components";
 import { addons, types } from "@storybook/addons";
 import { useParameter } from "@storybook/api";
+import { styled, css } from '@storybook/theming';
 
-addons.register("my/story-status", () => {
+export const statusToClass = {
+  deprecated: 'neutral',
+  development: 'critical',
+  review: 'warning',
+  published: 'positive'
+}
+
+addons.register("story-status/panel", () => {
   addons.add("story-status/panel", {
     title: "status",
     type: types.PANEL,
-    render: ({ active, key }) => (
-      <AddonPanel active={active} key={key}>
-        <Content />
-      </AddonPanel>
+    render: ({ active, key }) => {
+      return (
+        <AddonPanel active={active} key={key}>
+          <StatusDescription />
+        </AddonPanel>
+      );
+    }
+  });
+});
+
+addons.register("story-status/tool", api => {
+  addons.add("story-status/tool", {
+    title: 'status',
+    type: types.TOOL,
+    match: ({ viewMode }) => viewMode === 'story',
+    render: () => (
+      <Fragment>
+        <StatusBadgeWrapper />
+      </Fragment>
     )
   });
 });
 
-const Content = () => {
-  const results = useParameter("status", []); // story's parameter being retrieved here
+const StatusBadgeWrapper = () => {
+  const results = useParameter('status', '');
+  const statusBadgeClass = statusToClass[results];
 
   return (
-    <Fragment>
-      {results.length ? (
-        <ol>
-          {results.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ol>
-      ) : null}
-    </Fragment>
-  );
-};
+    results ?
+      <WithTooltip
+        position="bottom"
+        trigger="hover"
+        tooltip={({ onHide }) => 'Please do not use!'}
+        style={{display: 'inline-block', alignSelf: 'center'}}>
+        <StyledBadge status={statusBadgeClass}>
+          {results}
+        </StyledBadge>
+      </WithTooltip>
+    : null
+  )
+}
+
+const StyledBadge = styled(Badge)`
+  ${props =>
+    props.status === statusToClass['deprecated'] &&
+    css`
+      position: relative;
+      overflow: hidden;
+      border: 2px solid ${props.theme.color.critical};
+      z-index: 0;
+
+      &:before,
+      &:after {
+        content: '';
+        display: block;
+        position: absolute;
+        background: ${props.theme.color.critical};
+        width: 120%;
+        height: 2px;
+        top: 50%;
+        left: 50%;
+        z-index: -1;
+      }
+      &:before {
+        transform: translate3d(-50%, -50%, 0) rotateZ(-10deg);
+      }
+      &:after {
+        transform: translate3d(-50%, -50%, 0) rotateZ(10deg);
+      }
+    `};
+`;
+
+const StatusDescription = () => {
+  const results = useParameter('statusDescription', '');
+
+  return results;
+}
--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -18,6 +18,60 @@ export const emoji = () => (
 
 emoji.story = {
   parameters: {
-    status: ['deprecated']
+    status: 'deprecated',
+    statusDescription: `
+      # Don't ever use this component. It is broken!
+    `
+  }
+}
+
+export const moreEmoji = () => (
+  <Button onClick={action('clicked')}>
+    <span role="img" aria-label="so cool">
+      🔥🏀🏀🏀🔥
+    </span>
+  </Button>
+);
+
+moreEmoji.story = {
+  parameters: {
+    status: 'development',
+    statusDescription: `
+      # This is a work in progress or an experiment. Probably don't use it.
+    `
+  }
+}
+
+export const bowieMoji = () => (
+  <Button onClick={action('clicked')}>
+    <span role="img" aria-label="so cool">
+      🦄💅👩‍🎤🦄
+    </span>
+  </Button>
+);
+
+bowieMoji.story = {
+  parameters: {
+    status: 'review',
+    statusDescription: `
+      # This is in eng/design review. It's api may still change!
+    `
+  }
+}
+
+export const petMoji = () => (
+  <Button onClick={action('clicked')}>
+    <span role="img" aria-label="so cool">
+      🐈🐰🐶🐱
+    </span>
+  </Button>
+);
+
+petMoji.story = {
+  parameters: {
+    status: 'published',
+    statusDescription: `
+      # This component has been published! Use it 'til your heart's content!
+    `
   }
 }
